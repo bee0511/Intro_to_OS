d985eaef1f56881309b06091ce7c3ce2458cf781
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-11-01 14:38:16 +0800

    hw3-1 finish

---
+++ HW3 - Multithreading/auto.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+g++ -o tmp hw3-1.cpp -lpthread
+./tmp
+rm tmp
--- HW3 - Multithreading/hw3-1.cpp
+++ HW3 - Multithreading/hw3-1.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <semaphore.h>
 
 using namespace std;
+
+sem_t semaphore;
+int counter = 0;
 
 void count(int index) {
   static mutex io_mutex;
   int num = 1000000;
+  while(counter != index);
   while (num--) {}
   {
     lock_guard<mutex> lock(io_mutex);
     cout << "I'm thread " << index << ", local count: 1000000\n";
+    counter++;
   }
 }
 

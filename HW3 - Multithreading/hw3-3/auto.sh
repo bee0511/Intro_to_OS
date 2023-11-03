7be43eaea134fa2c7ff40e7383afdf947f0bd458
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-11-03 15:22:28 +0800

    finish 3-3 script

---
+++ HW3 - Multithreading/hw3-3/auto.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+case_num=3
+
+while getopts "t:" op 2>/dev/null; do
+  case $op in
+    t)
+      num_threads=$OPTARG
+      ;;
+    *)
+      echo "Default"
+      ;;
+  esac
+done
+
+g++ hw3-3_serial.cpp -o hw3-3 -lpthread
+time -p -o "$num_threads"threads.txt ./hw3-3 -t $num_threads < testcase/case"$case_num".txt
+time -p -o 1thread.txt ./hw3-3 -t 1 < testcase/case"$case_num".txt
+
+multi=$(cat "$num_threads"threads.txt | grep "real" | cut -c 6-)
+single=$(cat 1thread.txt | grep "real" | cut -c 6-)
+
+rm "$num_threads"threads.txt 1thread.txt
+# Check if $multi and $single are not empty
+if [ -n "$multi" ] && [ -n "$single" ]; then
+  echo "Total speed up is $(echo "$single/$multi" | bc -l)"
+else
+  echo "Error: Unable to calculate speed up. Check the 'real' output in time."
+fi
+
+rm hw3-3
--- HW3 - Multithreading/hw3-3/hw3-3_serial.cpp
+++ HW3 - Multithreading/hw3-3/hw3-3_serial.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstring>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
@@ -15,11 +18,38 @@ bool is_prime(int num) {
     return true;
 }
 
+void count_primes(int start, int end) {
+    int local_count = 0;
+    for (int i = start; i <= end; i++) {
+        if (is_prime(i)) local_count++;
+    }
+    global_count += local_count;
+}
+
 int main(int argc, char* argv[]) {
+    int num_threads = 1; // Default number of threads
+
+    // Parse command line arguments to get the number of threads
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
+            num_threads = atoi(argv[i + 1]);
+        }
+    }
+
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        if (is_prime(i)) global_count++;
+    // Divide the work among threads
+    vector<thread> threads;
+    int chunk_size = n / num_threads;
+    for (int i = 0; i < num_threads; i++) {
+        int start = i * chunk_size + 1;
+        int end = (i == num_threads - 1) ? n : (i + 1) * chunk_size;
+        threads.emplace_back(count_primes, start, end);
+    }
+
+    // Wait for all threads to finish
+    for (auto& t : threads) {
+        t.join();
     }
 
     cout << global_count << endl;

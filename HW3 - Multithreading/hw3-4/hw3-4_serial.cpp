801867f0cfe4c43560f80397f6a26ffa0bd5f8b4
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-11-04 14:23:52 +0800

    3-4 for loop version

--- HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
+++ HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <thread>
 #include <atomic>
+#include <mutex>
 #include <cstring>
 
 using namespace std;
@@ -11,40 +12,33 @@ int n, m;
 vector<uint64_t> subsets;
 uint64_t one = static_cast<uint64_t>(1);
 atomic<uint64_t> global_count(0);
+mutex count_mutex;
 
-void solve(int index, uint64_t current) {
-    try {
-        if (index == m) {
-            if (current == (one << n) - 1) global_count++;
-        } else {
-            solve(index + 1, current);
-            solve(index + 1, current | subsets[index]);
+void solve(int start, int end) {
+    uint64_t local_count = 0;
+
+    for (int i = start; i < end; i++) {
+        uint64_t current = 0;
+
+        for (int j = 0; j < m; j++) {
+            if ((i & (1 << j)) != 0) {
+                current |= subsets[j];
+            }
         }
-    } catch (const std::exception& e) {
-        std::cerr << "Exception caught: " << e.what() << std::endl;
-    }
-}
 
-
-void parallel_solve(int index, uint64_t current, int num_threads) {
-    if (index == m) {
-        if (current == (one << n) - 1) global_count++;
-    } else {
-        // Check if we have more work to do
-        if (index < m - num_threads + 1) {
-            thread t(parallel_solve, index + 1, current, num_threads);
-            parallel_solve(index + 1, current | subsets[index], num_threads);
-            t.join();
-        } else {
-            solve(index, current);
+        if (current == (one << n) - 1) {
+            local_count++;
         }
     }
+
+    lock_guard<mutex> lock(count_mutex);
+    global_count += local_count;
 }
 
 int main(int argc, char* argv[]) {
     int num_threads = 1; // Default number of threads
 
-    // Parse command line arguments to get the number of threads
+    // Parse command-line arguments to get the number of threads
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
             num_threads = atoi(argv[i + 1]);
@@ -63,7 +57,19 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    parallel_solve(0, 0, num_threads);
+    vector<thread> threads;
+    int chunk_size = (1 << m) / num_threads;
+    int start = 0;
+
+    for (int i = 0; i < num_threads; i++) {
+        int end = (i == num_threads - 1) ? (1 << m) : start + chunk_size;
+        threads.emplace_back(solve, start, end);
+        start = end;
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
 
     cout << global_count << endl;
     return 0;

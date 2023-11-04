c5d007d1616ac763418a093f384500d5ded9a0e4
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-11-04 15:44:23 +0800

    recursive, no speedup

--- HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
+++ HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
@@ -10,41 +10,30 @@ using namespace std;
 
 int n, m;
 vector<uint64_t> subsets;
-uint64_t one = static_cast<uint64_t>(1);
-atomic<uint64_t> global_count(0);
-mutex count_mutex;
+uint64_t one = static_cast<uint64_t>(1), global_count = 0;
+mutex global_count_mutex;
 
-void solve(int start, int end) {
-    uint64_t local_count = 0;
-
-    for (int i = start; i < end; i++) {
-        uint64_t current = 0;
-
-        for (int j = 0; j < m; j++) {
-            if ((i & (1 << j)) != 0) {
-                current |= subsets[j];
-            }
-        }
-
+void solve(int index, uint64_t current, int num_threads) {
+    if (index == m) {
         if (current == (one << n) - 1) {
-            local_count++;
+            lock_guard<mutex> lock(global_count_mutex);
+            global_count++;
+        }
+    } else {
+        if (num_threads > 1) {
+            thread t1([=]() { solve(index + 1, current, num_threads - 1); });
+            thread t2([=]() { solve(index + 1, current | subsets[index], num_threads - 1); });
+
+            t1.join();
+            t2.join();
+        } else {
+            solve(index + 1, current, 1);
+            solve(index + 1, current | subsets[index], 1);
         }
     }
-
-    lock_guard<mutex> lock(count_mutex);
-    global_count += local_count;
 }
 
-int main(int argc, char* argv[]) {
-    int num_threads = 1; // Default number of threads
-
-    // Parse command-line arguments to get the number of threads
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
-            num_threads = atoi(argv[i + 1]);
-        }
-    }
-
+int main(int argc, char *argv[]) {
     cin >> n >> m;
 
     subsets.resize(m);
@@ -57,19 +46,13 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    vector<thread> threads;
-    int chunk_size = (1 << m) / num_threads;
-    int start = 0;
-
-    for (int i = 0; i < num_threads; i++) {
-        int end = (i == num_threads - 1) ? (1 << m) : start + chunk_size;
-        threads.emplace_back(solve, start, end);
-        start = end;
+    int num_threads = 1;
+    if (argc > 1) {
+        num_threads = atoi(argv[1]);
     }
 
-    for (auto& t : threads) {
-        t.join();
-    }
+    thread t(solve, 0, 0, num_threads);
+    t.join();
 
     cout << global_count << endl;
     return 0;

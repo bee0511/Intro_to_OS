cf0491a7f9ca3092fcee00faaf30ed8e89c50a06
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-11-04 16:39:08 +0800

    modify auto

--- HW3 - Multithreading/hw3-3/auto.sh
+++ HW3 - Multithreading/hw3-3/auto.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 case_num=3
+filename=hw3-3_serial.cpp
 
 while getopts "t:" op 2>/dev/null; do
   case $op in
@@ -13,20 +14,28 @@ while getopts "t:" op 2>/dev/null; do
   esac
 done
 
-g++ hw3-3_serial.cpp -o hw3-3 -lpthread
-test -e hw3-3 || exit 255
-time -p -o "$num_threads"threads.txt ./hw3-3 -t $num_threads < testcase/case"$case_num".txt
-time -p -o 1thread.txt ./hw3-3 -t 1 < testcase/case"$case_num".txt
+g++ "$filename" -o tmp -lpthread
+test -e tmp || exit 255
+multi_ans=$(time -p -o "$num_threads"threads.txt ./tmp -t $num_threads < testcase/case"$case_num".txt)
+single_ans=$(time -p -o 1thread.txt ./tmp -t 1 < testcase/case"$case_num".txt)
+ans=$(cat answer/case"$case_num".txt)
+if [ "$multi_ans" -ne "$ans" ]; then
+  echo "Answer of multiple thread is wrong, the answer of multiple thread is $multi_ans"
+fi
+if [ "$single_ans" -ne "$ans" ]; then
+  echo "Answer of single thread is wrong, the answer of single thread is $single_ans"
+fi
 
 multi=$(cat "$num_threads"threads.txt | grep "real" | cut -c 6-)
 single=$(cat 1thread.txt | grep "real" | cut -c 6-)
-
+echo "Mutithread running time: $multi"
+echo "Singlethread running time: $single"
 rm "$num_threads"threads.txt 1thread.txt
-# Check if $multi and $single are not empty
-if [ -n "$multi" ] && [ -n "$single" ]; then
+
+if [ "$single" != "0.00" ]; then
   echo "Total speed up is $(echo "$single/$multi" | bc -l)"
 else
-  echo "Error: Unable to calculate speed up. Check the 'real' output in time."
+  echo "Error: Unable to calculate speed up."
 fi
 
-rm hw3-3
+rm tmp
--- HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
+++ HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
@@ -5,6 +5,8 @@
 #include <atomic>
 #include <mutex>
 #include <cstring>
+#include <functional>
+#include <queue>
 
 using namespace std;
 
@@ -12,6 +14,23 @@ int n, m;
 vector<uint64_t> subsets;
 uint64_t one = static_cast<uint64_t>(1), global_count = 0;
 mutex global_count_mutex;
+mutex queue_mutex;
+queue<function<void()>> task_queue;
+
+void worker_thread(int num_threads) {
+    while (true) {
+        function<void()> task;
+        {
+            lock_guard<mutex> lock(queue_mutex);
+            if (task_queue.empty()) {
+                break; // No more tasks, exit the thread
+            }
+            task = task_queue.front();
+            task_queue.pop();
+        }
+        task();
+    }
+}
 
 void solve(int index, uint64_t current, int num_threads) {
     if (index == m) {
@@ -21,11 +40,12 @@ void solve(int index, uint64_t current, int num_threads) {
         }
     } else {
         if (num_threads > 1) {
-            thread t1([=]() { solve(index + 1, current, num_threads - 1); });
-            thread t2([=]() { solve(index + 1, current | subsets[index], num_threads - 1); });
-
-            t1.join();
-            t2.join();
+            // Add tasks to the queue
+            {
+                lock_guard<mutex> lock(queue_mutex);
+                task_queue.push([=]() { solve(index + 1, current, num_threads - 1); });
+                task_queue.push([=]() { solve(index + 1, current | subsets[index], num_threads - 1); });
+            }
         } else {
             solve(index + 1, current, 1);
             solve(index + 1, current | subsets[index], 1);
@@ -51,8 +71,23 @@ int main(int argc, char *argv[]) {
         num_threads = atoi(argv[1]);
     }
 
-    thread t(solve, 0, 0, num_threads);
+    vector<thread> threads;
+    for (int i = 0; i < num_threads; i++) {
+        threads.push_back(thread(worker_thread, num_threads));
+    }
+
+    thread t([=]() { solve(0, 0, num_threads); });
+
     t.join();
+
+    // Add a sentinel task for each thread to exit
+    for (int i = 0; i < num_threads; i++) {
+        task_queue.push([]() {});
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
 
     cout << global_count << endl;
     return 0;

c9a7ce6d8f4f7ecb00f614cd8f7712dfdeb70170
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-11-05 19:48:13 +0800

    restore to for loop version

--- HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
+++ HW3 - Multithreading/hw3-4/hw3-4_serial.cpp
@@ -5,55 +5,46 @@
 #include <atomic>
 #include <mutex>
 #include <cstring>
-#include <functional>
-#include <queue>
 
 using namespace std;
 
 int n, m;
 vector<uint64_t> subsets;
-uint64_t one = static_cast<uint64_t>(1), global_count = 0;
-mutex global_count_mutex;
-mutex queue_mutex;
-queue<function<void()>> task_queue;
+uint64_t one = static_cast<uint64_t>(1);
+atomic<uint64_t> global_count(0);
+mutex count_mutex;
 
-void worker_thread(int num_threads) {
-    while (true) {
-        function<void()> task;
-        {
-            lock_guard<mutex> lock(queue_mutex);
-            if (task_queue.empty()) {
-                break; // No more tasks, exit the thread
+void solve(int start, int end) {
+    uint64_t local_count = 0;
+
+    for (int i = start; i < end; i++) {
+        uint64_t current = 0;
+
+        for (int j = 0; j < m; j++) {
+            if ((i & (1 << j)) != 0) {
+                current |= subsets[j];
             }
-            task = task_queue.front();
-            task_queue.pop();
         }
-        task();
-    }
-}
 
-void solve(int index, uint64_t current, int num_threads) {
-    if (index == m) {
         if (current == (one << n) - 1) {
-            lock_guard<mutex> lock(global_count_mutex);
-            global_count++;
+            local_count++;
         }
-    } else {
-        if (num_threads > 1) {
-            // Add tasks to the queue
-            {
-                lock_guard<mutex> lock(queue_mutex);
-                task_queue.push([=]() { solve(index + 1, current, num_threads - 1); });
-                task_queue.push([=]() { solve(index + 1, current | subsets[index], num_threads - 1); });
             }
-        } else {
-            solve(index + 1, current, 1);
-            solve(index + 1, current | subsets[index], 1);
-        }
-    }
+
+    lock_guard<mutex> lock(count_mutex);
+    global_count += local_count;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+    int num_threads = 1; // Default number of threads
+
+    // Parse command-line arguments to get the number of threads
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
+            num_threads = atoi(argv[i + 1]);
+        }
+    }
+
     cin >> n >> m;
 
     subsets.resize(m);
@@ -66,27 +57,18 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    int num_threads = 1;
-    if (argc > 1) {
-        num_threads = atoi(argv[1]);
-    }
-
     vector<thread> threads;
+    int chunk_size = (1 << m) / num_threads;
+    int start = 0;
+
     for (int i = 0; i < num_threads; i++) {
-        threads.push_back(thread(worker_thread, num_threads));
+        int end = (i == num_threads - 1) ? (1 << m) : start + chunk_size;
+        threads.emplace_back(solve, start, end);
+        start = end;
     }
 
-    thread t([=]() { solve(0, 0, num_threads); });
-
-    t.join();
-
-    // Add a sentinel task for each thread to exit
-    for (int i = 0; i < num_threads; i++) {
-        task_queue.push([]() {});
-    }
-
-    for (auto& thread : threads) {
-        thread.join();
+    for (auto& t : threads) {
+        t.join();
     }
 
     cout << global_count << endl;
